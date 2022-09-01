293c49fe152845231ae5592b35316d5223b3384d
Cain HC Chen <cain19811028@gmail.com>
2022-09-01 17:45:16 +0800

    chore: upgrade static files

--- main.go
+++ main.go
@@ -145,7 +145,7 @@ var indexTempl = template.Must(template.New("").Parse(`<!DOCTYPE html>
   <head>
     <title>Azure AD OAuth2 Example</title>
 
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body class="container-fluid">
     <div class="row">
@@ -164,7 +164,7 @@ var indexTempl = template.Must(template.New("").Parse(`<!DOCTYPE html>
       </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
     <script>
 {{with .Token}}
       var token = {{.}};
